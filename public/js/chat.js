8f04c333bb6f134a300c4b3980949620d547d98c
Ibrahim Abousalem <hima.abousalem@gmail.com>
2019-06-02 07:35:12 +0300

    sendLocation Feature and acknowledgement for bad words

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,1 +1,30 @@
-io()
+const socket = io()
+
+socket.on('message',(message)=>{
+    console.log(message)
+})
+
+document.querySelector('#message-form').addEventListener('submit',(e)=>{
+    e.preventDefault()
+    const message = e.target.elements.message.value
+    if(message == '')return
+    socket.emit('sendMessage', message, (error)=>{
+        if(error){
+            return console.log(error)
+        }
+        console.log('message delivered!')
+    })
+})
+
+document.querySelector('#send-location').addEventListener('click', ()=>{
+    if(!navigator.geolocation){
+        return alert('geolocation is not supported by your browser.')
+    }
+    navigator.geolocation.getCurrentPosition((position)=>{
+        //console.log(position)
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude, 
+            longitude: position.coords.longitude
+        })
+    })
+})
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require('path')
 const http = require('http')
 const express = require('express')
 const socktio = require('socket.io')
+const Filter = require('bad-words')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,8 +13,28 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection',()=>{
+io.on('connection',(socket)=>{
     console.log('New websocket connection!')
+    
+    socket.emit('message', 'Welcome!')
+    socket.broadcast.emit('A new user has joined!')
+
+    socket.on('sendMessage',(message, callback)=>{
+        const filter = new Filter()
+        if(filter.isProfane(message)){
+            return callback('profanity is not allowed!')
+        }
+        io.emit('message', message)
+        callback()
+    })
+    socket.on('sendLocation', (coords)=>{
+        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+    })
+    socket.on('disconnect',()=>{
+        // i used io because the user whose disconnectd not more being
+        //part of conversion so we can send this message to all users in chat room!
+        io.emit('message', 'A user has left!')
+    })
 })
 
 server.listen(port, ()=>{
