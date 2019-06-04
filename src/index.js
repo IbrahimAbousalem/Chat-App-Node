243c05dc6298f31633f76f4febe7eda194fb47c9
Ibrahim Abousalem <hima.abousalem@gmail.com>
2019-06-04 15:18:50 +0300

    styling the chat app and add login page

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ const http = require('http')
 const express = require('express')
 const socktio = require('socket.io')
 const Filter = require('bad-words')
+const {generateMessage, generateLocationMessage} = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,25 +17,25 @@ app.use(express.static(publicDirectoryPath))
 io.on('connection',(socket)=>{
     console.log('New websocket connection!')
     
-    socket.emit('message', 'Welcome!')
-    socket.broadcast.emit('A new user has joined!')
+    socket.emit('message', generateMessage('Welcome'))
+    socket.broadcast.emit('message',generateMessage('A new user has joined!'))
 
     socket.on('sendMessage',(message, callback)=>{
         const filter = new Filter()
         if(filter.isProfane(message)){
             return callback('profanity is not allowed!')
         }
-        io.emit('message', message)
+        io.emit('message', generateMessage(message))
         callback()
     })
     socket.on('sendLocation', (coords, callback)=>{
-        io.emit('message', `https://google.com/maps?q=${coords.latitude},${coords.longitude}`)
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${coords.latitude},${coords.longitude}`))
         callback()
     })
     socket.on('disconnect',()=>{
         // i used io because the user whose disconnectd not more being
         //part of conversion so we can send this message to all users in chat room!
-        io.emit('message', 'A user has left!')
+        io.emit('message', generateMessage('A user has left!'))
     })
 })
 
